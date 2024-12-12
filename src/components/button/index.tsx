c4527e34982090ad1e9678f6167fadac391f8e24
EsleySaab <esleysantana.sa@gmail.com>
2024-12-12 19:46:23 -0300

    creating the button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,34 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Text,
+  TextProps,
+  ActivityIndicator,
+} from "react-native"
+
+import { s } from "./styles"
+import { colors } from "@/styles/theme"
+
+type ButtonProps = TouchableOpacityProps & {
+  isLoading?: boolean
+}
+
+function Button({ children, style, isLoading = false }: ButtonProps) {
+  return (
+    <TouchableOpacity style={[s.container, style]}>
+      {isLoading ? (
+        <ActivityIndicator size="small" color={colors.gray[100]} />
+      ) : (
+        children
+      )}
+    </TouchableOpacity>
+  )
+}
+
+function Title({ children }: TextProps) {
+  return <Text style={s.title}>{children}</Text>
+}
+
+Button.Title = Title
+
+export { Button }
